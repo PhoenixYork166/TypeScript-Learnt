e1bdddf9583022e2552b901fbc420a3b9362ad01
root <phoenix166>
2023-12-08 02:09:55 -0500

    literals

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
 // =========== Literals ===================
-const combine = (input1: number | string, input2: number | string) => {
+var combine = function (input1, input2) {
     //let result: number | string;
-    let result;
+    var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
-    } else {
+    }
+    else {
         result = input1.toString() + input2.toString();
     }
     return result;
-}
-
-const combinedAges = combine(30, 26);
-console.log(`combinedAges: ${combinedAges}`);
-
-const combinedNames = combine('Max', 'Anna');
-console.log(`combinedNames: ${combinedNames}`);
-
-
-
-
-
-
-
-
-
-
-
-
+};
+var combinedAges = combine(30, 26);
+console.log("combinedAges: ".concat(combinedAges));
+var combinedNames = combine('Max', 'Anna');
+console.log("combinedNames: ".concat(combinedNames));
 // const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
 //     const result = n1 + n2;
-
 //     if (showResult) {
 //         console.log(phrase + result);
-
 //     } else {
 //         return result;
 //     }
 // }
-
 // const number1 = 5;
 // const number2 = 10;
 // const result = true;
