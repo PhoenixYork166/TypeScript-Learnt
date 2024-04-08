bcda605095cba7ca7765ae80e027c8757d7431f7
phoenixyork166 <berlinlee2017@gmail.com>
2024-04-08 12:14:40 +0800

    8 Apr 2024

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,17 @@
 "use strict";
 class Department {
-    // private id: string;
-    //public readonly name: string;
-    //private employees: string[] = [];
     constructor(id, name, employees = []) {
         this.id = id;
         this.name = name;
         this.employees = employees;
         this.name = name;
+        console.log(`Department.fiscalYear:\n${Department.fiscalYear}`);
+    }
+    // static method allow access of Department.createEmployee()
+    // without instantiating e.g.
+    // const businessDepartment = new Department(id, name, employees[])
+    static createEmployee(name) {
+        return { name: name };
     }
     describe() {
         console.log(`Department.id: ${this.id}`);
@@ -22,6 +26,13 @@ class Department {
         console.log(`current this.employees:\n${this.employees}`);
     }
 }
+// private id: string;
+//public readonly name: string;
+//private employees: string[] = [];
+// allowing classes inheriting this class Department
+// to amend this.employees
+//protected employees: string[] = [];
+Department.fiscalYear = 2024;
 // Class Inheritance
 class ITDepartment extends Department {
     constructor(id, admins) {
@@ -34,17 +45,46 @@ class ITDepartment extends Department {
     }
 }
 class AccountingDepartment extends Department {
+    // getter method
+    // making lastReport accessible to this class
+    // using closures hoisting
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No latest report found');
+    }
+    // setter method
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
     constructor(id, reports) {
         super(id, 'Accounting');
         this.reports = reports;
+        this.lastReport = reports[reports.length - 1];
+    }
+    addEmployee(name) {
+        const forbidden = 'Max';
+        if (name === forbidden) {
+            console.log(`forbidden: ${forbidden} cannot be added`);
+            return;
+        }
+        this.employees.push(name);
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(`this.reports:\n${this.reports}`);
     }
 }
+const employee1 = Department.createEmployee('Max2');
+console.log(`const employee1 = Department.createEmployee('Max')`);
+console.log(employee1, Department.fiscalYear);
 // constructor(id: number, admins: string[]) {...}
 const it = new ITDepartment(1, ['Berlin']);
 it.addEmployee('Max');
@@ -56,12 +96,19 @@ it.printEmployeeInformation();
 console.log(`\n`);
 // constructor(id: number, private reports: string[]) {...}
 const accounting = new AccountingDepartment(2, []);
+// using setter method to edit private class.fieldValue
+accounting.mostRecentReport = 'Year End Report';
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
+accounting.addEmployee('Berlin');
 accounting.describe();
 accounting.printEmployeeInformation();
 accounting.addReport('Something went wrong...');
 accounting.printReports();
+// accessing getter method 
+// get mostRecentReport() {...}
+// in class AccountingDepartment
+console.log(`accounting.mostRecentReport:\n${accounting.mostRecentReport}`);
 // creating a new Department object 'Accounting'
 // using Department blueprint
 // const accounting = new Department(1, 'Accounting');
--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,27 @@ class Department {
     //public readonly name: string;
     //private employees: string[] = [];
 
+    // allowing classes inheriting this class Department
+    // to amend this.employees
+    //protected employees: string[] = [];
+
+    static fiscalYear = 2024;
+
     constructor(
         public id: number,
-        public name: string, 
-        private employees: string[] = []
+        public name: string,
+        protected employees: string[] = []
         ) {
         this.name = name;
+
+        console.log(`Department.fiscalYear:\n${Department.fiscalYear}`);
+    }
+
+    // static method allow access of Department.createEmployee()
+    // without instantiating e.g.
+    // const businessDepartment = new Department(id, name, employees[])
+    static createEmployee(name: string) {
+       return { name: name };
     }
 
     describe(this: Department) {
@@ -43,19 +58,54 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    // getter method
+    // making lastReport accessible to this class
+    // using closures hoisting
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No latest report found');
+    }
+
+    // setter method
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
 
     constructor(id: number, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[reports.length - 1];
+    }
+
+    addEmployee(name: string) {
+        const forbidden: string = 'Max';
+
+        if (name === forbidden) {
+            console.log(`forbidden: ${forbidden} cannot be added`);
+            return;
+        }
+        this.employees.push(name);
     }
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
         console.log(`this.reports:\n${this.reports}`);
     }
 }
+
+const employee1 = Department.createEmployee('Max2');
+console.log(`const employee1 = Department.createEmployee('Max')`);
+console.log(employee1, Department.fiscalYear);
 
 // constructor(id: number, admins: string[]) {...}
 const it = new ITDepartment(1, ['Berlin']);
@@ -70,12 +120,20 @@ console.log(`\n`);
 
 // constructor(id: number, private reports: string[]) {...}
 const accounting = new AccountingDepartment(2, []);
+
+// using setter method to edit private class.fieldValue
+accounting.mostRecentReport = 'Year End Report';
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
+accounting.addEmployee('Berlin');
 accounting.describe();
 accounting.printEmployeeInformation();
 accounting.addReport('Something went wrong...');
 accounting.printReports();
+// accessing getter method 
+// get mostRecentReport() {...}
+// in class AccountingDepartment
+console.log(`accounting.mostRecentReport:\n${accounting.mostRecentReport}`);
 
 // creating a new Department object 'Accounting'
 // using Department blueprint
