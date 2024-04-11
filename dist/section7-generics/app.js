375ca059b418627bba510e2898d45a3c9787921f
phoenixyork166 <berlinlee2017@gmail.com>
2024-04-11 17:28:14 +0800

    11 Apr 2024 - section7-generics

---
+++ dist/section7-generics/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// function merge1<T, U>(objA: T, objB: U) 
+// ** this says objA & objB will often be of different types **
+// ** adding type constraints **
+function merge1(objA, objB) {
+    return Object.assign({}, objA, objB);
+}
+;
+// if NO Type Casting => cannot access mergedObj.properties
+// ** This allows objA to be of dynamic object structure **
+// const mergedObj1 = merge1({ name: 'Max', hobbies: ['Sports] }, {age: 100});
+const mergedObj1 = merge1({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj1);
+console.log(`mergedObj.name`);
+console.log(mergedObj1.name);
+console.log(`mergedObj.age`);
+console.log(mergedObj1.age);
