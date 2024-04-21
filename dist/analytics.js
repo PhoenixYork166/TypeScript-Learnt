08bdf0bfeb8cf8094074a8915fb3aa19c782bde8
phoenixyork166 <berlinlee2017@gmail.com>
2024-04-21 23:33:04 +0800

    21-Apr-2024 removed obsolete folders

--- dist/analytics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let logged;
-const sendAnalytics = (data) => {
-    console.log(`sending data:\n${data}`);
-    logged = true;
-    console.log(`logged: ${logged}`);
-};
-sendAnalytics('The Data');
